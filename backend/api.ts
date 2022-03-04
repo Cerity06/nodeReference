df8b3de954e6d3dca884fe19fd16a76b32e1dafe
cerity06 <jonathan.mekdoul@gmail.com>
2022-03-04 09:58:15 -0500

    express guide

--- backend/api.ts
+++ backend/api.ts
@@ -1,25 +1,47 @@
-import { Request, Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
 import fs from 'fs';
 import path from 'path';
-import express from 'express';
 
-const app = express();
+const app = require('./middleware');
+
+type User = {
+  id: number;
+  first_name: string;
+  last_name: string;
+  email: string;
+  gender: string;
+  ip_address: string;
+};
 
 // TOP LEVEL CODE (read once)
 const res = fs.readFileSync(
   path.join(process.cwd(), '/data/data.json'),
   'utf-8'
 );
-const data: string = JSON.parse(res.toString());
+const data: User[] = JSON.parse(res.toString());
 
-// Functions
-const getUserData = (req: Request, res: Response) => {
+// Functions used in routes
+const getAllUsers = (req: Request, res: Response, next: NextFunction) => {
+  console.log('Request received for all users');
   res.status(200).json({ message: 'success', data: data });
 };
 
-// ROUTING WITH APP
-app.use(express.json());
+const getUserData = (req: Request, res: Response) => {
+  const userId = req.params.id;
+  const userData = data.filter((user) => {
+    return user.id.toString() === userId;
+  });
+  if (userData.length > 0) {
+    res.status(200).json({ message: 'success', data: userData });
+  } else {
+    res
+      .status(404)
+      .json({ message: 'fail', data: 'no data found for this user' });
+  }
+};
 
-app.get('/', getUserData);
+// ROUTING
+app.route('/').get(getAllUsers);
+app.get('/:id?', getUserData);
 
 module.exports = app;
--- backend/server.ts
+++ backend/server.ts
@@ -1,6 +1,5 @@
 const app = require('./api');
-
-const port = '8080';
+const port = process.env.PORT ?? 8080;
 const hostname = '127.0.0.1';
 
 app.listen(port, hostname, () => {
